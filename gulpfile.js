364464cb3670538747e2febf4b2f7b53d33d03fe
Maxime Bertonnier <m.bertonnier@gmail.com>
2017-09-08 17:08:25 +0200

    WIP feat(gulp): many many features
    - Tâche de copie des assets
    - Minification des images
    - Source maps (js et css)
    - Configuration de l’ES6
    - Tâche de build / dev (dossiers prod/dev séparés)

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,14 @@ var FILENAMES = {
 var PATHS = {
     dest: {
         global: './dist',
-        img: './dist/img'
+        imgFolder: 'img',
+        prodFolder: 'prod',
+        devFolder: 'dev'
     },
     src: {
-        assets: './src/assets/*',
+        assets: ['src/assets/**', '!src/assets/{img,img/**}'],
         html: './src/html/*.html',
-        img: './src/img/*',
+        img: './src/assets/img/*',
         jsCode: './src/js/app/*.js',
         jsVendor: './src/js/vendor/*.js',
         scss: './src/scss/*.scss'
@@ -24,49 +26,86 @@ var PATHS = {
 var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     babel = require('gulp-babel'),
+    babelify = require('babelify'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     cssnano = require('cssnano'),
+    del = require('del'),
     imagemin = require('gulp-imagemin'),
     livereload = require('gulp-livereload'),
     mqpacker = require('css-mqpacker'),
     pngquant = require('imagemin-pngquant'),
     postcss = require('gulp-postcss'),
+    rename = require('gulp-rename'),
     sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify');
 
-// TODO
-// Tâches de copie des assets (tout sauf images qui ont leur propre tâche)
-// Source maps
-// Tâche de build
-// Vendor CSS
-// Dossier JS 'specifics' ?
-
-gulp.task('html', function () {
+function html () {
     return gulp.src(PATHS.src.html)
         .pipe(livereload());
-});
+}
+gulp.task('html', html);
 
-gulp.task('concatJsVendor', function () {
+function JSVendor (opts) {
+    var targetFolder = opts.type === 'changed' ? PATHS.dest.devFolder : PATHS.dest.prodFolder;
     return gulp.src(PATHS.src.jsVendor)
         .pipe(concat(FILENAMES.jsVendor))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest(PATHS.dest.global))
+        .pipe(gulp.dest(PATHS.dest.global + '/' + targetFolder))
         .pipe(livereload());
-});
+}
+gulp.task('JSVendor', JSVendor);
 
-gulp.task('concatJsCode', function () {
-    return gulp.src(PATHS.src.jsCode)
-        .pipe(concat(FILENAMES.jsCode))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest(PATHS.dest.global))
-        .pipe(livereload());
-    });
+function JSCode (opts) {
+    if (opts.type === 'changed') {
+        // Development mode
+        return gulp.src(PATHS.src.jsCode)
+            .pipe(sourcemaps.init())
+            .pipe(browserify({ transform: ['babelify'] }))
+            .pipe(babel())
+            .pipe(concat(FILENAMES.jsCode))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(PATHS.dest.global + '/' + PATHS.dest.devFolder))
+            .pipe(livereload());
+    } else {
+        // Production mode
+        return gulp.src(PATHS.src.jsCode)
+            .pipe(browserify({ transform: ['babelify'] }))
+            .pipe(babel())
+            .pipe(concat(FILENAMES.jsCode))
+            .pipe(uglify())
+            .pipe(gulp.dest(PATHS.dest.global + '/' + PATHS.dest.prodFolder));
+    }
+}
+gulp.task('concatJsCode', JSCode);
 
-gulp.task('css', function () {
+function copyAssets (opts) {
+    var folder = opts.type === 'changed' ? PATHS.dest.devFolder : PATHS.dest.prodFolder;
+    return gulp.src(PATHS.src.assets)
+        .pipe(gulp.dest(PATHS.dest.global + '/' + folder));
+}
+gulp.task('copyAssets', copyAssets);
+
+function cleanDist (opts) {
+    var folder = opts.type === 'changed' ? PATHS.dest.devFolder : PATHS.dest.prodFolder;
+    return del(PATHS.dest.global + '/' + folder);
+}
+gulp.task('cleanDist', cleanDist);
+
+function minifyImages (opts) {
+    var destFolder = PATHS.dest.global + '/' + (opts.type === 'changed' ? PATHS.dest.devFolder : PATHS.dest.prodFolder) + '/' + PATHS.dest.imgFolder;
+
+    return gulp.src(PATHS.src.img)
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5})
+        ]))
+        .pipe(gulp.dest(destFolder));
+}
+gulp.task('minifyImages', minifyImages);
+
+function css (opts) {
     var postcssPlugins = [
         mqpacker({
             sort: true
@@ -83,14 +122,26 @@ gulp.task('css', function () {
         })
     ];
 
-    return gulp.src(PATHS.src.scss)
-        .pipe(sass())
-        .pipe(postcss(postcssPlugins))
-        .pipe(gulp.dest(PATHS.dest.global))
-        .pipe(livereload());
-});
+    if (opts.type === 'changed') {
+        // Development mode
+        return gulp.src(PATHS.src.scss)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(postcss(postcssPlugins))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(PATHS.dest.global + '/dev'))
+            .pipe(livereload());
+    } else {
+        // Production mode
+        return gulp.src(PATHS.src.scss)
+            .pipe(sass())
+            .pipe(postcss(postcssPlugins))
+            .pipe(gulp.dest(PATHS.dest.global + '/prod'));
+    }
+}
+gulp.task('css', css);
 
-gulp.task('images', function () {
+function images () {
     return gulp.src(PATHS.src.img)
         .pipe(imagemin({
             progressive: true,
@@ -100,14 +151,25 @@ gulp.task('images', function () {
             use: [pngquant()]
         }))
         .pipe(gulp.dest(PATHS.dest.img));
-});
+}
+gulp.task('images', images);
 
-gulp.task('watch', function () {
-    // // Launches tasks once before watching (creates files if needed)
-    gulp.start('css');
-    gulp.start('concatJsVendor');
-    gulp.start('concatJsCode');
-    gulp.start('html');
+gulp.task('dev', function () {
+    var devOptions = { type: 'changed' };
+    // Launches tasks once before watching (creates files if needed)
+    cleanDist(devOptions)
+        .then(function () {
+            minifyImages(devOptions);
+            copyAssets(devOptions);
+            minifyImages(devOptions);
+            copyAssets(devOptions);
+            css(devOptions);
+            JSVendor(devOptions);
+            JSCode(devOptions);
+            html(devOptions);
+        }).catch(function (err) {
+            throw new err;
+        });
 
     if (ENABLE_LIVE_RELOAD) {
         livereload.listen({
@@ -115,43 +177,38 @@ gulp.task('watch', function () {
         });
     }
 
-    gulp.watch(PATHS.src.jsVendor, ['concatJsVendor']);
-    gulp.watch(PATHS.src.jsCode, ['concatJsCode']);
-    gulp.watch(PATHS.src.scss, ['css']);
-    gulp.watch(PATHS.src.html, ['html']);
+    gulp.watch(PATHS.src.jsVendor, function (opts) { JSVendor(opts); });
+    gulp.watch(PATHS.src.jsCode, function (opts) { JSCode(opts); });
+    gulp.watch(PATHS.src.scss, function (opts) { css(opts); });
+    gulp.watch(PATHS.src.assets, function (opts) { copyAssets(opts); });
+    gulp.watch(PATHS.src.img, function (opts) { minifyImages(opts); });
+    gulp.watch(PATHS.src.html, function (opts) { html(opts); });
 });
 
 /**
  * Production task
  */
-gulp.task('build', function () {
-    gulp.src(PATHS.src.scss)
-        .pipe(sass())
-        // TODO: add CSS task
-        .pipe(gulp.dest(PATHS.dest.global));
-
-    gulp.src(PATHS.src.jsVendor)
-        .pipe(concat(FILENAMES.jsVendor))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(PATHS.dest.global));
-
-    gulp.src(PATHS.src.jsCode)
-        .pipe(concat(FILENAMES.jsCode))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(PATHS.dest.global));
-
-    if (ENABLE_IMAGE_COMPRESSION) {
-        gulp.start('images');
-    }
+gulp.task('prod', function () {
+    var prodOptions = { type: '' };
+    
+    // cleaningTask
+    cleanDist(prodOptions)
+        .then(function () {
+            minifyImages(prodOptions);
+            copyAssets(prodOptions);
+            css(prodOptions);
+            JSVendor(prodOptions);
+            JSCode(prodOptions);
+        })
+        .catch(function (err) {
+            throw new err;
+        });
 });
+
+gulp.task('watch', ['dev']);
+gulp.task('build', ['prod']);
 
 /**
  * Default task
  */
-gulp.task('default', ['css', 'concatJsVendor', 'concatJsCode', 'images'], function () {});
+gulp.task('default', ['css', 'JSVendor', 'concatJsCode', 'images'], function () {});
